b63624083e6ea6af28beced72a9154016fcb22a6
Jeffrey <104775056+yutupa@users.noreply.github.com>
2024-10-08 09:48:54 +0800

    Add files via upload

---
+++ TQC+_Python301-310/PYD303.py
@@ -0,0 +1,6 @@
+n = eval(input())
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print("%4d" %(j*i), end="")
+    print("")
---
+++ TQC+_Python301-310/PYD304.py
@@ -0,0 +1,9 @@
+n = eval(input())
+
+sum = 0
+
+for i in range(1, n+1):
+    if i % 5 == 0:
+        sum += i
+
+print(sum)
