5394965645147c727418711f4fb48b92ea3d9ce2
Jeffrey <104775056+yutupa@users.noreply.github.com>
2024-10-14 02:25:06 +0800

    Add files via upload

--- TQC+_Python_401-410/PYD410.py
+++ TQC+_Python_401-410/PYD410.py
@@ -1,11 +1,28 @@
 n = eval(input())
-t = 1+2*(n-1)
 
 for i in range(n):
-    for j in range(t):
-        if (j-i) > t/2:
-            print(' ', end="")
-        else:
-            print('*', end="")
-    print()
+    for j in range(n - i - 1):
+        print(' ', end="")
+    for j in range((2 * i + 1)):
+        print('*', end="")
 
+    print()
+    
+
+'''
+for i in range(n):
+    for j in range((2*n-1)):
+        if j < (n - 1):
+            if (n - i - j -2) >= 0:
+                print(' ', end="")
+            else:
+                print('*', end="")
+        elif j == (n - 1):
+            print('*', end="")
+        else:
+            if (n + i - j - 1) < 0:
+                print('', end="")
+            else:
+                print('*', end="")
+    print()
+'''
