d9149cedcd67f9d32c0eb11ff586905ffcdfddd7
Jeffrey <104775056+yutupa@users.noreply.github.com>
2024-10-13 17:05:06 +0800

    Add files via upload

--- TQC+_Python_401-410/PYD401.py
+++ TQC+_Python_401-410/PYD401.py
@@ -1,1 +1,15 @@
+a = []
 
+for i in range(10):
+    a.append(eval(input()))
+
+'''
+minimum = a[0]
+for i in range(10):
+    if a[i] < minimum:
+        minimum = a[i]
+
+print(minimum)
+'''
+
+print(min(a))
