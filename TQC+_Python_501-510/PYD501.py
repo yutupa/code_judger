d1b015c7d6e60cb2bd2bf3f10809965fc1e3b87d
Jeffrey <104775056+yutupa@users.noreply.github.com>
2024-10-20 15:35:45 +0800

    Add files via upload

--- TQC+_Python_501-510/PYD501.py
+++ TQC+_Python_501-510/PYD501.py
@@ -1,1 +1,9 @@
+def compute():
+    Department = input()
+    StudentID = input()
+    Name = input()
+    print('Department: %s' %Department)
+    print('Student ID: %s' %StudentID)
+    print('Name: %s' %Name)
 
+compute()
