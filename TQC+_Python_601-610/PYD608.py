54e742e5147115c79b21e855b4dc593677c4c689
Jeffrey <104775056+yutupa@users.noreply.github.com>
2024-10-22 18:45:33 +0800

    Add files via upload

--- TQC+_Python_601-610/PYD608.py
+++ TQC+_Python_601-610/PYD608.py
@@ -1,8 +1,30 @@
-arr = [[0]*3]*3
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+import numpy as np
+# arr = [[0]*3]*3 #trap
+arr = [ [0]*3 for i in range(3)]
 
 for i in range(3):
     for j in range(3):
         arr[i][j] = eval(input())
+        
+max = arr[0][0]
+max_index_i = 0
+max_index_j = 0
+min = arr[0][0]
+min_index_i = 0
+min_index_j = 0
 
-print(max(arr), arr.index(max(arr)))
-print(min(arr), arr.index(min(arr)))
+for rows in arr:
+    for cols in rows:
+        if cols > max:
+            max = cols
+            max_index_i = arr.index(rows)
+            max_index_j = rows.index(cols)
+        if cols < min:
+            min = cols
+            min_index_i = arr.index(rows)
+            min_index_j = rows.index(cols)
+
+print("max: %d in (%d, %d)" %(max, max_index_i, max_index_j))
+print("min: %d in (%d, %d)" %(min, min_index_i, min_index_j))
