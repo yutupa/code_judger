bba15f9b72a03e203dfb38e16d6f278854875a61
Jeffrey <104775056+yutupa@users.noreply.github.com>
2024-10-29 11:38:20 +0800

    Add files via upload

--- TQC+_Python_701-710/PYD701.py
+++ TQC+_Python_701-710/PYD701.py
@@ -1,1 +1,11 @@
-
+LIST = []
+num = eval(input())
+while num!=-9999:
+    LIST.append(num)
+    num = eval(input())
+TUPLE = tuple(LIST)
+print(TUPLE)
+print('Length:',len(TUPLE))
+print('Max:',max(TUPLE))
+print('Min:',min(TUPLE))
+print('Sum:',sum(TUPLE))
